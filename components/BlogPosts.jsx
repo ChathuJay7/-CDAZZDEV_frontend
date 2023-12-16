6242f9d83448c5153c67dd8b7348443e36781e54
ChathuJay7 <chathurajmhb7@gmail.com>
2023-12-16 13:36:29 +0530

    Add delete post function

--- components/BlogPosts.jsx
+++ components/BlogPosts.jsx
@@ -1,68 +1,87 @@
 
-import React from 'react'
+import React from "react";
 //import RemoveBtn from './RemoveBtn'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation';
-import { useSelector } from 'react-redux';
-import { HiPencilAlt } from 'react-icons/hi'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+import { HiOutlineTrash, HiPencilAlt } from "react-icons/hi";
 
-const getPosts = async() => {
-    try{
-      const res = await fetch('http://localhost:5000/post/',
-        {
-          cache: "no-store"
-        }
-      )
-  
-      if(!res.ok){
-        throw new Error("Failed to fetch todos");
-      }
-  
-      return res.json();
-  
-    } catch(error){
-      console.log("Error loading todos: ", error)
+const getPosts = async () => {
+  try {
+    const res = await fetch("http://localhost:5000/post/", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch todos");
     }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading todos: ", error);
   }
+};
 
 const BlogPosts = async () => {
+  const { user } = useSelector((state) => state.auth);
+  const router = useRouter();
 
-    const { user } = useSelector((state) => state.auth);
-    const router = useRouter();
+  const { posts } = await getPosts();
 
-    
-      const {posts} = await getPosts();
+    const handleRemovePost = async(id) => {
+        const confirmed = confirm("Are you sure?");
 
-    return (
-        <>
-        <div className='flex justify-end mt-5 mb-5 '>
-            <Link className="bg-slate-600 text-white hover:bg-white hover:text-slate-600 border border-slate-600 rounded-md px-6 py-3 font-bold" href={'/addPost'}>+ Add Post</Link>
-        </div>
-        {posts.map((p) => (
-            // eslint-disable-next-line react/jsx-key
-            <div key={p._id} className='p-3 bg-slate-200 border border-slate-300 hover:bg-white my-3 flex justify-between gap-5 items-center'>
-              <div>
-                <h2 className='font-bold text-2xl'>{p.title}</h2>
-                <div>{p.description}</div>
-              </div>
-              
-              <div className='flex gap-2'>
-                {/* <RemoveBtn id={p._id} /> */}
-                {/* {user._id === p.userId && (
+        if(confirmed) {
+            const res = await fetch(`http://localhost:5000/post/${id}`, {
+                method: "DELETE"
+            })
+            
+            if(res.ok) {
+                router.refresh();
+            }
+        }
+    }
+
+  return (
+    <>
+      <div className="flex justify-end mt-5 mb-5 ">
+        <Link
+          className="bg-slate-600 text-white hover:bg-white hover:text-slate-600 border border-slate-600 rounded-md px-6 py-3 font-bold"
+          href={"/addPost"}
+        >
+          + Add Post
+        </Link>
+      </div>
+      {posts.map((p) => (
+        // eslint-disable-next-line react/jsx-key
+        <div
+          key={p._id}
+          className="p-3 bg-slate-200 border border-slate-300 hover:bg-white my-3 flex justify-between gap-5 items-center"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{p.title}</h2>
+            <div>{p.description}</div>
+          </div>
+
+          <div className="flex gap-2">
+            {/* <RemoveBtn id={p._id} /> */}
+            {/* {user._id === p.userId && (
                     
                 <Link href={`/editTodo/${p._id}`}>
                   <HiPencilAlt size={24} />
                 </Link>
                 )} */}
-                <Link href={`/editPost/${p._id}`}>
-                    <HiPencilAlt size={24} />
-                </Link>
-                
-              </div>
-            </div>
-          ))}
-        </>
-      )
-}
+            <button onClick={() => handleRemovePost(p._id)} className="text-red-400">
+              <HiOutlineTrash size={24} />
+            </button>
+            <Link href={`/editPost/${p._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default BlogPosts
+export default BlogPosts;
--- components/EditPostForm.jsx
+++ components/EditPostForm.jsx
@@ -37,7 +37,7 @@ export default function EditPPostForm({ id, title, description }) {
     <form className='flex flex-col gap-5 mt-5 justify-center items-center bg-slate-200 py-10' onSubmit={handleSubmit}>
         <h1 className='text-center font-bold text-2xl'>Update Post</h1>
         <input className='border border-slate-500 px-8 py-2 w-[80%]' onChange={(e) => setNewTitle(e.target.value)} value={newTitle} type='text' placeholder='Topic Title'/>
-        <input className='border border-slate-500 px-8 py-2 w-[80%]' onChange={(e) => setNewDescription(e.target.value)} value={newDescription} type='text' placeholder='Topic Description'/>
+        <textarea className='border border-slate-500 px-8 py-2 w-[80%]' onChange={(e) => setNewDescription(e.target.value)} value={newDescription} type='text' placeholder='Topic Description'/>
         <button type='submit' className='bg-slate-600 text-white hover:bg-white hover:text-slate-600 border border-slate-600 font-bold px-6 py-3 w-[80%]'>Update Post</button>
     </form>
   )
